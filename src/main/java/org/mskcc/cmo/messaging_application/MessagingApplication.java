244cd53eba999dcd373ac8c06612aea3f02629ee
Benjamin Gross <benjamin.gross@gmail.com>
2020-10-28 22:01:41 -0400

    Prototyping

---
+++ src/main/java/org/mskcc/cmo/messaging_application/MessagingApplication.java
@@ -0,0 +1,49 @@
+package org.mskcc.cmo.messaging_application;
+
+import org.mskcc.cmo.messaging.Gateway;
+import org.mskcc.cmo.messaging.MessageConsumer;
+import org.mskcc.cmo.messaging.model.SampleMetadata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "org.mskcc.cmo.messaging")
+public class MessagingApplication implements CommandLineRunner, MessageConsumer
+{
+    @Autowired
+    @Qualifier("NATSGateway")
+    private Gateway messagingGateway;
+
+    @Override
+    public void run(String... args)
+    {
+        try
+        {
+            //messagingGateway.initialize("nats://dashi-dev.cbio.mskcc.org:4222");
+            messagingGateway.subscribe("NEW_SAMPLE", SampleMetadata.class, this);
+            SampleMetadata sm = new SampleMetadata();
+            sm.sampleID = "sampleID here";
+            messagingGateway.publish("NEW_SAMPLE", sm);
+            Thread.sleep(5000);
+            messagingGateway.shutdown();
+        }
+        catch(Exception e) {}
+    }
+
+    @Override
+    public void onMessage(Object message)
+    {
+        SampleMetadata sampleMetadata = (SampleMetadata)message;
+        System.out.println("*** Sample Metadata received ***");
+        System.out.println("SampleID: " + sampleMetadata.sampleID);
+        System.out.println("***                          ***");
+    }
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(MessagingApplication.class, args);
+    }
+}
